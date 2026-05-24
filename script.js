5797c95fa7c2a5f4f54b4f7acf3abef3187f7200
elnathval <elnathval@outlook.com>
2026-05-25 00:58:51 +0200

    Versión final creo

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ var text;
 var files = [];
 var filaActual = [];
 // Amb fetch si el tens en un servidor
+
+
+
+
+
+
+function randomWord() {
+    filaActual = files[Math.floor(Math.random() * files.length)];
+    if(document.getElementById(filaActual[2]).checked) {
+        if((filaActual[2] === "infinitiu" || filaActual[2] === "gerundi") || document.getElementById(filaActual[3]).checked) {
+        document.getElementById("paraula").textContent = filaActual[7];
+        } else {
+            randomWord();
+        }
+    } else {
+        randomWord();
+    }
+}
+
 const res = fetch('verbs_valencians_pau_valencia.csv').then(response => response.text()).then(data => {
   //console.log(data);
   text = data;
   files = text.split("\n").slice(1).map(line => line.split(","));
   filaActual = files[Math.floor(Math.random() * files.length)];
-    document.getElementById("paraula").textContent = filaActual[7];  
+  document.getElementById("paraula").textContent = filaActual[7];  
 });
 
 
@@ -21,7 +40,7 @@ function comprovar() {
     console.log(temps, conjugacio, persona, nombre, mode);
     console.log(filaActual[3], filaActual[1], filaActual[4], filaActual[5], filaActual[2]);
 
-    if(mode === "infinitiu" && mode == "gerundi") {
+    if(filaActual[2] === "infinitiu" || filaActual[2] === "gerundi") {
         if(mode === filaActual[2] && conjugacio === filaActual[1]){
             document.getElementById("resposta").textContent = "Correcte!";
         }
@@ -30,7 +49,7 @@ function comprovar() {
             return;
         }
     }
-    else if(mode === "participi") {
+    else if(filaActual[2] === "participi") {
         if(mode === filaActual[2] && conjugacio === filaActual[1] && nombre  === filaActual[5]){
             document.getElementById("resposta").textContent = "Correcte!";
         }
@@ -39,7 +58,7 @@ function comprovar() {
             return;
         }
     }
-    else if(mode === "imperatiu") {
+    else if(filaActual[2] === "imperatiu") {
         if(mode === filaActual[2] && conjugacio === filaActual[1] && persona === filaActual[4] && nombre === filaActual[5]){
             document.getElementById("resposta").textContent = "Correcte!";
         }
@@ -58,6 +77,6 @@ function comprovar() {
         }
     }
 
-    filaActual = files[Math.floor(Math.random() * files.length)];
-    document.getElementById("paraula").textContent = filaActual[7];  
+    randomWord(); 
 }
+
