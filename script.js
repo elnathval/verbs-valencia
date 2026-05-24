62fd815ef078331bd05d719a55947db3cbf90feb
elnathval <elnathval@outlook.com>
2026-05-25 00:08:22 +0200

    Beta

---
+++ script.js
@@ -0,0 +1,63 @@
+var text;
+var files = [];
+var filaActual = [];
+// Amb fetch si el tens en un servidor
+const res = fetch('verbs_valencians_pau_valencia.csv').then(response => response.text()).then(data => {
+  //console.log(data);
+  text = data;
+  files = text.split("\n").slice(1).map(line => line.split(","));
+  filaActual = files[Math.floor(Math.random() * files.length)];
+    document.getElementById("paraula").textContent = filaActual[7];  
+});
+
+
+function comprovar() {
+    var temps = document.getElementById("temps").value;
+    var conjugacio = document.getElementById("conjugacio").value;
+    var persona = document.getElementById("persona").value;
+    var nombre = document.getElementById("nombre").value;
+    var mode = document.getElementById("mode").value;
+
+    console.log(temps, conjugacio, persona, nombre, mode);
+    console.log(filaActual[3], filaActual[1], filaActual[4], filaActual[5], filaActual[2]);
+
+    if(mode === "infinitiu" && mode == "gerundi") {
+        if(mode === filaActual[2] && conjugacio === filaActual[1]){
+            document.getElementById("resposta").textContent = "Correcte!";
+        }
+        else {
+            document.getElementById("resposta").textContent = `Incorrecte! El verb complet és: ${filaActual[1]} conjugació, ${filaActual[2]}`;
+            return;
+        }
+    }
+    else if(mode === "participi") {
+        if(mode === filaActual[2] && conjugacio === filaActual[1] && nombre  === filaActual[5]){
+            document.getElementById("resposta").textContent = "Correcte!";
+        }
+        else {
+            document.getElementById("resposta").textContent = `Incorrecte! El verb complet és: ${filaActual[1]} conjugació, ${filaActual[2]}, ${filaActual[5]}`;
+            return;
+        }
+    }
+    else if(mode === "imperatiu") {
+        if(mode === filaActual[2] && conjugacio === filaActual[1] && persona === filaActual[4] && nombre === filaActual[5]){
+            document.getElementById("resposta").textContent = "Correcte!";
+        }
+        else {
+            document.getElementById("resposta").textContent = `Incorrecte! El verb complet és: ${filaActual[1]} conjugació, ${filaActual[2]}, ${filaActual[4]} persona, ${filaActual[5]}`;
+            return;
+        }
+    }
+    else {
+        if(mode === filaActual[2] && conjugacio === filaActual[1] && temps === filaActual[3] && persona === filaActual[4] && nombre === filaActual[5]){
+            document.getElementById("resposta").textContent = "Correcte!";
+        }
+        else {
+            document.getElementById("resposta").textContent = `Incorrecte! El verb complet és: ${filaActual[1]} conjugació, ${filaActual[3]} del ${filaActual[2]}, ${filaActual[4]} persona, ${filaActual[5]} `;
+            return;
+        }
+    }
+
+    filaActual = files[Math.floor(Math.random() * files.length)];
+    document.getElementById("paraula").textContent = filaActual[7];  
+}
